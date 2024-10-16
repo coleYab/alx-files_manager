0c5e99f4764ee1787eb893442270f03980535318
coleYab <yabume123@gmail.com>
2024-10-16 18:12:01 -0400

    add: added a db and redis client implementation

--- utils/db.js
+++ utils/db.js
@@ -1,5 +1,5 @@
 import mongodb from 'mongodb';
-import envLoader from './env_loader';
+// import envLoader from './env_loader';
 // eslint-disable-next-line no-unused-vars
 import Collection from 'mongodb/lib/collection';
 
@@ -11,7 +11,7 @@ class DBClient {
    * Creating a new constructor.
    */
   constructor() {
-    envLoader();
+   // envLoader();
     const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || 27017;
     const database = process.env.DB_DATABASE || 'files_manager';
